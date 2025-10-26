fa12d9f977e8cd88b962d70ff6d9543cb8557af4
Caiolorrance <153196880+Caiolorrance@users.noreply.github.com>
2025-10-26 22:22:35 +0000

    Add files via upload

---
+++ funcionarios.py
@@ -0,0 +1,149 @@
+"""
+• __init__(self, nome, idade, salario): inicializa gerente.
+• mostrar_informacoes(self): sobrescreve método e mostra salário + bônus.
+• gerar_relatorio(self, funcionarios): percorre lista e imprime dados de cada um.
+"""
+from pessoa import Pessoa
+
+
+class Funcionario(Pessoa):
+    Lista_funcionarios=[]
+    def __init__(self, nome, idade,salario):
+        super().__init__(nome, idade)
+        self.salario=salario
+        
+       
+
+class Gerente(Funcionario):
+    def __init__(self, nome="", idade=0, salario=2100):
+        super().__init__(nome, idade, salario)
+    
+
+    def mostrar_informacoes(self):
+        print(f"Nome: {self.nome} Idade: {self.idade} Salario: {self.salario} Bônus: {self.salario*0.1}")
+
+    def gerar_relatorio(self):
+        print("\n===== RELATÓRIO DE FUNCIONÁRIOS =====")
+        if not Funcionario.Lista_funcionarios:
+            print("Nenhum funcionário registrado.")
+        else:
+            total_salarios = 0
+            for func in Funcionario.Lista_funcionarios:
+                func.mostrar_informacoes()
+                total_salarios += func.salario
+
+            print("--------------------------------------")
+            print(f"Total de funcionários: {len(Funcionario.Lista_funcionarios)}")
+            print(f"Folha salarial total: R$ {total_salarios:.2f}")
+            print(f"Folha com bônus do gerente: R$ {total_salarios + (self.salario * 0.1):.2f}")
+        print("======================================\n")
+
+"""
+__init__(self, nome, id_func, idade, salario, turno): adiciona turno.
+• @property turno; retorna o turno atual.
+• @turno.setter: altera o turno (valores válidos: manhã, tarde, noite).
+• mostrar_informacoes(self): sobrescreve método, incluindo o turno.
+• registrar_hospede(self, hospede, lista_hospedes): adiciona hóspede à lista.
+• listar_hospedes(self, lista_hospedes): percorre lista e mostra hóspedes.
+• registrar
+"""
+
+class Recepcionista(Funcionario):
+    
+    lista_hospedes=[]
+    def __init__(self, nome, idade, salario, id_func, turno):
+        super().__init__(nome, idade, salario,)
+
+        self.id_func=id_func
+        self._turno=turno
+        
+
+
+    @property
+    def turno(self):
+        return self._turno
+    
+    @turno.setter
+    def turno(self):
+        op=input("Escolha o turno de trabalho:\n1-Manhã\n2-Tarde\n3-Noite")
+        if op==1:
+            self._turno="Manhâ"
+
+        if op==2:
+            self._turno="Tarde"
+
+        else:
+            self._turno="Noite"
+    
+    def mostrar_informacoes(self):
+        print(f"ID funcionario: {self.id_func} Nome do funcionario: {self.nome} idade: {self.idade} turno:{self.turno} salario: {self.salario}")
+    
+    def registrar_hospede(self):
+
+        while True:
+            self.nome = input("1 - Parar cadastro\nNome: ")
+            if self.nome == "1":
+                break
+            idade = input("Idade: ")
+            dias_estadia = input("Dias de estadia: ")
+           
+            
+            from pessoa import Hospede
+
+            h = Hospede(self.nome, idade, dias_estadia)
+            self.lista_hospedes.append(h)
+                
+        print("\nRegistro de Hospedes encerrado!")
+
+    def listar_hospedes(self):
+        for hospede in self.lista_hospedes:
+                print(f" - Hospedes registrados: {hospede.nome}, Idade: {hospede.idade}, Dias de estadia: {hospede.dias_estadia} quarto: {hospede.quarto}")
+
+
+
+"""
+• __init__(self, nome, idade, salario, especialidade): define área técnica.
+• @property especialidade: retorna especialidade.
+mostrar_informacoes(self): sobrescreve método, incluindo a especialidade.
+"""
+class TecnicoManutencao(Funcionario):
+    def __init__(self, nome, idade, salario=1350, especialidade="indefinido"):
+        super().__init__(nome, idade, salario,)
+
+        self.especialidade=especialidade
+
+        @property
+        def especialidade(self):
+            return self.especialidade
+        
+        def mostrar_informacoes(self):
+            print(f"Nome: {self.nome} Idade: {self.idade} Salario: {self.salario} Especialidade: {self.especialidade}")
+
+
+
+"""
+• __init__(self, nome, id_fun, salario, setor, especialidade)
+• registrar_hospede(nome_hospede): de Recepcionista
+• registrar_reparo(descricao): de TecnicoManutencao
+• mostrar_info(): sobrescrito (exibe dados de ambas as funções)
+"""
+
+class TecnicoRecepcao(Recepcionista,TecnicoManutencao):
+    def __init__(self, nome="", idade=0, salario=1400, id_func="indefinido", turno="indefinido",setor="indefinido", especialidade="indefinido"):
+        super().__init__(nome, idade, salario, id_func, turno,)
+
+        self.setor=setor
+        self.especialidade=especialidade
+
+    def registrar_hospede(self):
+        hospede=Recepcionista()
+        hospede.registrar_hospede()
+    
+    def registrar_reparo(descricao):
+        descricao=input("Descreva o reparo a ser realizado: ") 
+        print(f"Reparo registrado: {descricao}")
+        
+
+    def mostrar_informacoes(self):
+        print(f"ID funcionario: {self.id_func} Nome do funcionario: {self.nome} idade: {self.idade} turno:{self.turno} salario: {self.salario} Setor: {self.setor} Especialidade: {self.especialidade}")
+        
---
+++ quartos.py
@@ -0,0 +1,46 @@
+"""
+__init__(self, numero, tipo, preco_diaria): inicializa quarto.
+• @property ocupado: retorna se o quarto está ocupado.
+• @property preco_diaria: retorna o preço por diária.
+• @preco_diaria.setter: impede preços negativos.
+• mostrar_informacoes(self): mostra tipo, preço e status.
+"""
+#PRA QUE O ABC?????
+from abc import ABC
+class Quarto(ABC):
+    def __init__(self, numero="indefinido", tipo="indefinido", preco_diaria=0):
+        super().__init__()
+
+        self.numero=numero
+        self.tipo=tipo
+        self._preco_diaria=preco_diaria
+    
+    @property
+    def ocupado(self):
+        return self._ocupado
+    
+    @property
+    def preco_diaria(self):
+        return self._preco_diaria
+    
+    @preco_diaria.setter
+    def preco_diaria(self,valor):
+        if valor<0:
+            print("preço da diaria não pode ser negativo!")
+        else:
+            self._preco_diaria=valor
+    
+    def mostrar_informacoes(self):
+        status="Ocupado" if self.ocupado else "Disponível"
+        print(f"Nº do quarto: {self.numero}, Tipo: {self.tipo}, Preço da diária: {self.preco_diaria}, Status: {status}")
+
+class QuartoSimples(Quarto):
+    def __init__(self, numero, preco_diaria):
+        super().__init__(numero, "Simples", preco_diaria)
+        self._ocupado=False
+
+class QuartoLuxo(Quarto):
+    def __init__(self, numero, preco_diaria):
+        super().__init__(numero, "Luxo", preco_diaria)
+        self._ocupado=False
+
